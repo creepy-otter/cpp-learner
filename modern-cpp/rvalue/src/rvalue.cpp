badb7588d42b1f5965ee69df48cea268417e2631
creepy-otter <creepyotter.github@gmail.com>
2022-09-26 01:24:30 +0000

    [cpp-basic][class-constructor] Add constructor content

--- modern-cpp/rvalue/src/rvalue.cpp
+++ modern-cpp/rvalue/src/rvalue.cpp
@@ -3,15 +3,16 @@
 class Vec {
   int _n;
   int *_buf;
+
 public:
-  Vec(int n): _n(n) {
+  Vec(int n) : _n(n) {
     std::cout << "Vec(int n)" << std::endl;
     _buf = new int[_n];
     for (int i = 0; i < _n; i++) {
       _buf[i] = 0;
     }
   }
-  Vec(const Vec& v) {
+  Vec(const Vec &v) {
     std::cout << "Vec(const Vec& v)" << std::endl;
     _n = v._n;
     _buf = new int[_n];
@@ -19,14 +20,15 @@ public:
       _buf[i] = 0;
     }
   }
-  Vec(Vec&& v) {
+  Vec(Vec &&v) {
     std::cout << "Vec(Vec&& v)" << std::endl;
     _n = v._n;
     _buf = v._buf;
     v._buf = nullptr;
   }
-  friend Vec operator+(const Vec& a, const Vec& b) {
-    std::cout << "friend Vec operator+ (const Vec& a, const Vec& b)" << std::endl;
+  friend Vec operator+(const Vec &a, const Vec &b) {
+    std::cout << "friend Vec operator+ (const Vec& a, const Vec& b)"
+              << std::endl;
     if (a._n != b._n) {
       std::cout << "Dimension not match!" << std::endl;
       exit(1);
@@ -37,8 +39,9 @@ public:
     }
     return res;
   }
-  friend Vec operator+(Vec&& a, const Vec& b) {
-    std::cout << "friend Vec operator+ (const Vec&& a, const Vec& b)" << std::endl;
+  friend Vec operator+(Vec &&a, const Vec &b) {
+    std::cout << "friend Vec operator+ (const Vec&& a, const Vec& b)"
+              << std::endl;
     if (a._n != b._n) {
       std::cout << "Dimension not match!" << std::endl;
       exit(1);
@@ -47,7 +50,7 @@ public:
     for (int i = 0; i < res._n; i++) {
       res._buf[i] = res._buf[i] + b._buf[i];
     }
-    return res; 
+    return res;
   }
   ~Vec() {
     std::cout << "~Vec()" << std::endl;
@@ -61,6 +64,7 @@ public:
 
 int main() {
   Vec v1(3), v2(3), v3(3), v4(3);
-  Vec v5 = v1 + v2 + v3 + v4; // v1 + v2 create an anonymous object, which is a r-value.
+  // v1 + v2 create an anonymous object, which is a r-value.
+  Vec v5 = v1 + v2 + v3 + v4;
   return 0;
 }
